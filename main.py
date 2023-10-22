6ec18996e8be7789cfd755d4bbce0e9be97327d9
Sulav Devkota <Sulav@Owner-no-MacBook-Air.local>
2023-10-22 18:51:45 +0545

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f"Insert, {name}")  # to toggle the breakpoint.
+input("")
+
+# Press the green button in the gutter to run the script.
+if __name__ == "__main__":
+    print_pd("")
+
+# See PyCharm at
+if __name__ == "__secondary__":
+     print_nm(f'Product:{name} + Price:{price} + Quantity:{qnty}')
+
+if __name__ == "__primary__":
+    print_
