9515d663901b947c26ab4ff9ea11998411c3005e
Sulav Devkota <Sulav@Owner-no-MacBook-Air.local>
2023-11-22 15:20:26 +0545

    contact

--- home/models.py
+++ home/models.py
@@ -30,3 +30,11 @@ class SiteInfo(models.Model):
         return self.address1
 
 
+class Contact(models.Model):
+    name = models.CharField(max_length=300)
+    email = models.EmailField(max_length=200)
+    subject = models.TextField()
+    message = models.TextField()
+
+    def __str__(self):
+        return self.name
--- home/views.py
+++ home/views.py
@@ -15,7 +15,20 @@ def about(request):
 def contact(request):
     views = {}
     views['contact_info'] = SiteInfo.objects.all()
-
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        subject = request.POST['subject']
+        message = request.POST['message']
+        data = Contact.objects.create(
+            name = name,
+            email = email,
+            subject = subject,
+            message = message
+        )
+        data.save()
+        views['success_messages'] = "The form is submitted"
+        return render(request, 'contact.html', views)
     return render(request, 'contact.html', views)
 
 def portfolio(request):
